57e8b59e97b91ac5aa5e00a28dcd19d8541e5d31
Mortimer333 <mortimer333@vp.pl>
2018-06-05 08:12:06 +0200

    Civ6 Wit3

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,21 @@
 function poe() {
     window.open("HTML/poe.html","_parent");
 }
+function civ6() {
+    window.open("HTML/civ6.html","_parent");
+}
+function wit3() {
+    window.open("HTML/wit3.html","_parent");
+}
+function dbl3() {
+    window.open("HTML/dbl3.html","_parent");
+}
+function fable() {
+    window.open("HTML/fable.html","_parent");
+}
+function fallout() {
+    window.open("HTML/fallout.html","_parent");
+}
+function witch() {
+    window.open("HTML/witch.html","_parent");
+}
